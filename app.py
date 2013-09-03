b155a3040cd07d3ae9754254cb0ff76cfe09ce38
Daniel Zappala <daniel.zappala@gmail.com>
2013-09-03 11:55:28 -0600

    Fixed 404 handling.

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ from flask_frozen import Freezer
 from config import app, freezer
 
 @app.route('/',defaults={'directory':None,'page':'index'})
+@app.route('/<page>',defaults={'directory':None})
 @app.route('/<path:directory>/',defaults={'page':'index'})
 @app.route('/<path:directory>/<page>')
 def show(directory,page):
-    if not directory:
-        return render_template(page + '.html')
-    return render_template(directory+"/" + page + '.html')
-    
+    try:
+        if not directory:
+            return render_template(page + '.html')
+        return render_template(directory+"/" + page + '.html')
+    except:
+        return render_template('404.html'), 404
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
+@freezer.register_generator
+def custom404():
+    yield '/404'
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
--- dodo.py
+++ dodo.py
@@ -1,5 +1,5 @@
 def task_install():
     """install """
     return {
-        'actions': ['python app.py build','rsync -rav --delete build/* zappala@cs360.byu.edu:/var/www/cs360.byu.edu'],
+        'actions': ['python app.py build','cp htaccess build/.htaccess','rsync -rav --delete build/* zappala@cs360.byu.edu:/var/www/cs360.byu.edu'],
         }
